8db9db0b3c87fe7f3eb958db0139228a833eb62e
Michael Diver <michael.diver@collinsongroup.com>
2018-06-29 17:52:52 +0100

    Base network infra

--- terraform.tf
+++ terraform.tf
@@ -16,3 +16,73 @@ resource "aws_subnet" "centralmanagement1c"{
 	}
 
 }
+
+resource "aws_subnet" "centraldevops1a"{
+	availability_zone = "eu-west-1a"
+	vpc_id = "${aws_vpc.management.id}"
+	cidr_block = "10.200.83.0/24"
+	tags {
+		Name = "CentralDevops1a"
+	}
+
+}
+
+resource "aws_subnet" "centralmanagement1a"{
+	availability_zone = "eu-west-1a"
+	vpc_id = "${aws_vpc.management.id}"
+	cidr_block = "10.200.80.0/24"
+	tags {
+		Name = "CentralManagement1a"
+	}
+
+}
+
+resource "aws_subnet" "centralpublicaccess1b"{
+	availability_zone = "eu-west-1b"
+	vpc_id = "${aws_vpc.management.id}"
+	cidr_block = "10.200.85.0/24"
+	tags {
+		Name = "CentralPublicAccess1b"
+	}
+
+}
+
+resource "aws_subnet" "centralmanagement1b"{
+	availability_zone = "eu-west-1b"
+	vpc_id = "${aws_vpc.management.id}"
+	cidr_block = "10.200.81.0/24"
+	tags {
+		Name = "CentralManagement1b"
+	}
+
+}
+
+resource "aws_subnet" "centralpublicaccess1a"{
+	availability_zone = "eu-west-1a"
+	vpc_id = "${aws_vpc.management.id}"
+	cidr_block = "10.200.84.0/24"
+	tags {
+		Name = "CentralPublicAccess1a"
+	}
+
+}
+
+resource "aws_subnet" "centralpublicaccess1c"{
+	availability_zone = "eu-west-1c"
+	vpc_id = "${aws_vpc.management.id}"
+	cidr_block = "10.200.86.0/24"
+	tags {
+		Name = "CentralPublicAccess1c"
+	}
+
+}
+
+
+
+#IGW
+resource "aws_internet_gateway" "CentralManagement" {
+	vpc_id = "${aws_vpc.management.id}"
+	tags {
+		Name = "centralManagement"
+	}
+}
