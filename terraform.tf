4182b72bded2f1da12be4eeae4453b0b565b4cd2
Michael Diver <michael.diver@collinsongroup.com>
2018-06-29 17:50:43 +0100

    8th

--- terraform.tf
+++ terraform.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "management" {
     Name = "Group Management VPC"
   }
 }
+
+#Subnet
+resource "aws_subnet" "centralmanagement1c"{
+	availability_zone = "eu-west-1c"
+	vpc_id = "${aws_vpc.management.id}"
+	cidr_block = "10.200.82.0/24"
+	tags {
+		Name = "CentralManagement1c"
+	}
+
+}
